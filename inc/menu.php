83d03ed6acbc7cfd338ec0bcba3790cacbfb6096
Arissatur Rohman <oonaries@gmail.com>
2020-05-26 19:09:09 +0700

    menambahkan file tracking

---
+++ inc/menu.php
@@ -0,0 +1,30 @@
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+
+      <a class="navbar-brand" href="#">Appel Sukodono</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pelayanan</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Riwayat</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link btn btn-outline-secondary" data-toggle="modal" data-target="#modalLoginForm"
+              href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
